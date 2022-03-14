75b35f3442860495c068d98dc655c655b3c86737
eiknaem <98448736+eiknaem@users.noreply.github.com>
2022-03-14 20:25:57 +0700

    Add files via upload

---
+++ methavin-project-profile/js_data.js
@@ -0,0 +1,24 @@
+/*
+    Name Methavin Ukrisussadon ID:1630903555
+/*
+
+/* JavaScript Functions myname */
+function myname() {
+    document.getElementById("name").innerHTML = "Methavin Ukrisussadon";
+    }
+/* JavaScript Functions myid */
+function myid() {
+    document.getElementById("id").innerHTML = "1639093555";
+    }
+/* JavaScript Functions mystudy */
+function mystudy() {
+    document.getElementById("study").innerHTML = "Bangkok University";
+    }    
+/* JavaScript Functions mymajor */
+function mymajor() {
+    document.getElementById("major").innerHTML = "Engineering (Computer and Robotics Engineering)";
+    }
+/* JavaScript Functions myhobby */
+function myhobby() {
+    document.getElementById("hobby").innerHTML = "Play Game , Listen Music , Watch Movie , Exercise";
+    }
